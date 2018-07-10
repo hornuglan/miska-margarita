f8e6effd407fa7cb3fd1897f15967c18ca0f9a12
hornuglan <hornuglan@gmail.com>
2018-07-10 22:30:43 +0300

    Добавлены контейнеры для содержимого, исправлены стили, добавлен скрипт для выпадающего меню

---
+++ source/js/menu.js
@@ -0,0 +1,16 @@
+let navMenu = document.querySelector(".main-menu");
+let navButton = document.querySelector(".main-menu__button-icon");
+
+
+navMenu.classList.remove("main-menu--nojs");
+
+navButton.addEventListener('click', function () {
+  if (navMenu.classList.contains("main-menu--closed")) {
+    navMenu.classList.add("main-menu--opened");
+  } else {
+    navMenu.classList.add("main-menu--closed");
+  }
+});
+
+
+
