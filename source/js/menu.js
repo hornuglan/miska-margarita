f873fd5d73d905d8c0b9582b324914a52fe05255
hornuglan <hornuglan@gmail.com>
2018-07-24 00:04:32 +0300

    Добавлены стили для планшетной и десктопной версий

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,16 +1,12 @@
 let navMenu = document.querySelector(".main-menu");
 let navButton = document.querySelector(".main-menu__button-icon");
 
-
 navMenu.classList.remove("main-menu--nojs");
 
 navButton.addEventListener('click', function () {
   if (navMenu.classList.contains("main-menu--closed")) {
-    navMenu.classList.add("main-menu--opened");
+    navMenu.classList.remove("main-menu--closed");
   } else {
     navMenu.classList.add("main-menu--closed");
   }
 });
-
-
-
